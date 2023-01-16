83b0c00d8cc93066a387eed2b0f1bb67904bcccb
Roy <roy@onbrand.co.uk>
2023-01-16 22:05:50 +0000

    Reduce winner confetti duration

--- src/App.js
+++ src/App.js
@@ -310,8 +310,8 @@ class App extends React.Component {
         // confetti function
       });*/
 
-      // do this for 30 seconds
-      var duration = 30 * 1000;
+      // do this for 10 seconds
+      var duration = 10 * 1000;
       var end = Date.now() + duration;
 
       (function frame() {
