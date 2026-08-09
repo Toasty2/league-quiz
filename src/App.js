4f271405e37986906d3590213b0c74ff9a352c56
Roy Wenman <doihaveto@gmail.com>
2026-08-09 23:48:11 +0100

    Format timer as time instead of a number

--- src/App.js
+++ src/App.js
@@ -103,7 +103,11 @@ class App extends React.Component {
   }
 
   formatElapsedTime = (totalMs) => {
-    return (totalMs / 1000).toFixed(3);
+    var totalSeconds = Math.floor(totalMs / 1000);
+    var minutes = Math.floor(totalSeconds / 60);
+    var seconds = totalSeconds % 60;
+    var milliseconds = totalMs % 1000;
+    return `${minutes}:${String(seconds).padStart(2, '0')}.${String(milliseconds).padStart(3, '0')}`;
   }
 
   calculateFinalScore = () => {
