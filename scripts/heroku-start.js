b0ca2142371f0a9ccb7141e4293ca00319994ec2
Roy Wenman <doihaveto@gmail.com>
2026-08-11 02:33:55 +0100

    Add comression to improve performance

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(compression());
 app.use(express.json());
 // Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')));
